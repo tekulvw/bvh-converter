c3c2952da98d481993f588178e109496a7337ee7
Will Tekulve <tekulve.will@gmail.com>
2016-11-16 23:52:02 -0500

    remove dependencies on my code

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import sys
 import csv
 
 from cgkit.bvh import BVHReader
-import cgkit.bvh
-from joint import Joint
-from skeleton import Skeleton
 from cgkit_skeleton import process_bvhfile, process_bvhkeyframe
 import logging
 
-cgkit.bvh.Node = Joint
 log = logging.getLogger("converter")
 
 """
@@ -19,21 +15,6 @@ Notes:
  - For each frame we have to recalculate from root
  - End sites are semi important (used to calculate length of the toe? vectors)
 """
-
-
-class Converter(BVHReader, object):
-    def __init__(self, skeleton, *args, **kwargs):
-        super(Converter, self).__init__(*args, **kwargs)
-        self.skeleton = skeleton
-        self.count = 0
-
-    def onHierarchy(self, root):
-        self.skeleton._set_hierarchy(root)
-
-    def onFrame(self, values):
-        if self.count < 5:
-            self.skeleton._add_frame(values)
-            self.count += 1
 
 
 def setup_logger(level):
@@ -60,22 +41,15 @@ if __name__ == "__main__":
     setup_logger(logging.DEBUG)
     fname = sys.argv[1]
     print("Input filename: {}".format(fname))
-    # s = Skeleton()
-    # c = Converter(s, fname)
-    # c.read()
 
     other_s = process_bvhfile(fname)
-    for i in range(5):
+    for i in range(len(other_s.keyframes)):
         new_frame = process_bvhkeyframe(other_s.keyframes[i], other_s.hips,
                                         other_s.dt * i)
-        # other_s.keyframes[i] = new_frame
 
     with open("output.csv", 'w') as f:
         writer = csv.writer(f, lineterminator="\n")
-        header, frames = other_s.get_frames(n=1)
+        header, frames = other_s.get_frames()
         writer.writerow(header)
         for frame in frames:
             writer.writerow(frame)
-    # for j in s._joints:
-    #   log.debug("{}: {}".format(j.name, j.total_offset))
-    # c.dump_frames()
